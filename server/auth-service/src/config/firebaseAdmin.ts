1a36a757d475fc8f9fc74de09c84775fc4d20cfe
vizzscript <vratik.zade@pinnacle.in>
2026-01-14 21:09:12 +0530

    fix: change credentials json to base64 string and called from env

--- server/auth-service/src/config/firebaseAdmin.ts
+++ server/auth-service/src/config/firebaseAdmin.ts
@@ -1,11 +1,17 @@
-import admin from "firebase-admin";
-import serviceAccount from "../config/firebase-service-account.json";
+import admin from 'firebase-admin';
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    projectId: serviceAccount.project_id, // 🔥 IMPORTANT
-  });
+const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+if (!base64) {
+  throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_BASE64');
 }
+
+const serviceAccount = JSON.parse(
+  Buffer.from(base64, 'base64').toString('utf8')
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 export default admin;
